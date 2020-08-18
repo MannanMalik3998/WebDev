cda4e17524ea63a8cea146a0002a96c014c87b1a
Abdul Mannan <manan.hameed47@gmail.com>
2020-08-18 10:26:05 +0500

    Update InstallingNpmPackages.js

--- Node/NodePackage(readline-sync)/InstallingNpmPackages.js
+++ Node/NodePackage(readline-sync)/InstallingNpmPackages.js
@@ -1,4 +1,5 @@
-//npm install readline-sync package installed
+//package installed
+//" npm install readline-sync " 
 
 var package = require("readline-sync");
 var name = package.question("What is your name: ");
